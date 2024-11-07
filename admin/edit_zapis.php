ba94efd98f8e6d77275bbc95d62f13e5cbe2de04
pascal.jura <boucnik.jiri@gmail.com>
2024-11-07 20:10:42 +0100

    možnost úpravy čísla dokumentu

--- admin/edit_zapis.php
+++ admin/edit_zapis.php
@@ -24,6 +24,7 @@ if (isset($_GET['id_zapis']) && is_numeric($_GET['id_zapis'])) {
     if ($result->num_rows > 0) {
         $row = $result->fetch_assoc();
         $datum = date('Y-m-d', strtotime($row['datum']));
+        $cislo_dokumentu = $row['cislo_dokumentu']; // Načtení čísla dokumentu
         $zapis = $row['zapis'];
         $zapis = str_replace("=", "\n", $zapis);
         $textInLomitkach = ziskatTextVLomitkach($zapis);
@@ -50,10 +51,13 @@ function getSklonovanyText($text)
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $id_zapis = $_POST["id_zapis"];
     $datum = $_POST["datum"];
+    $cislo_dokumentu = $_POST["cislo_dokumentu"]; // Načtení čísla dokumentu
     $zapisText = $_POST["zapis"];
     $zapisText = str_replace(["\r\n", "\r", "\n"], "=", $zapisText);
     $zapisText = nahraditMarkdown($zapisText);
-    $sql = "UPDATE zapis SET datum='$datum', zapis='$zapisText' WHERE id_zapis = $id_zapis";
+
+    // Aktualizace záznamu v databázi včetně čísla dokumentu
+    $sql = "UPDATE zapis SET datum='$datum', cislo_dokumentu='$cislo_dokumentu', zapis='$zapisText' WHERE id_zapis = $id_zapis";
     if ($conn->query($sql) === TRUE) {
         header("Location: show_zapis.php?id_zapis=$id_zapis");
         exit();
@@ -61,6 +65,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         echo "Chyba při aktualizaci záznamu: " . $conn->error;
     }
 }
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -84,17 +89,26 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <div class="table-heading">
             <?php echo '&#x1F499;・ Úprava zápisu・2023/2024'; ?>
         </div>
-        <form action="" method="post" id="myForm" style="width: 80%; max-width: 400px; margin-bottom: 5px;">
+        <form action="" method="post" id="myForm" style="width: 80%; max-width: 400px; margin-bottom: 5px; ">
             <input type="hidden" name="id_zapis" value="<?php echo $id_zapis; ?>">
+
             <label for="datum" style="font-size: 16px; margin-bottom: 8px;">Datum:</label>
             <input type="date" name="datum" id="datum" value="<?php echo $datum; ?>"
-                style="width: 100%; padding: 10px; margin-bottom: 16px; border: 1px solid #ccc; border-radius: 5px; box-sizing: border-box;"
+                style="width: 100%; padding: 10px; margin-bottom: 16px; border: 1px solid #ccc; border-radius: 5px; box-sizing: border-box; font-family: Calibri, sans-serif;"
                 required>
+
+            <!-- Nové pole pro Číslo dokumentu -->
+            <label for="cislo_dokumentu" style="font-size: 16px; margin-bottom: 8px;">Číslo dokumentu:</label>
+            <input type="text" name="cislo_dokumentu" id="cislo_dokumentu" value="<?php echo $cislo_dokumentu; ?>"
+                style="width: 100%; padding: 10px; margin-bottom: 16px; border: 1px solid #ccc; border-radius: 5px; box-sizing: border-box; font-family: Calibri, sans-serif;"
+                required>
+
             <label for="zapis" style="font-size: 16px; margin-bottom: 8px;">Zápis:</label>
             <textarea name="zapis" id="zapis" rows="10"
-                style="width: 100%; padding: 10px; margin-bottom: 5px; border: 1px solid #ccc; border-radius: 5px; box-sizing: border-box; white-space: nowrap;"
+                style="width: 100%; padding: 10px; margin-bottom: 5px; border: 1px solid #ccc; border-radius: 5px; box-sizing: border-box; white-space: nowrap; font-family: Calibri, sans-serif;"
                 required><?php echo $zapis; ?></textarea>
         </form>
+
         <div class="button-container" id="buttonContainer">
             <button type="submit" form="myForm">Uložit změny</button>
             <a href="show_zapis.php?id_zapis=<?php echo $id_zapis; ?>"><button>Opustit stránku beze změn</button></a>
