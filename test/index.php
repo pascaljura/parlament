06ea04e3fedcf7f7a3e30fb124e0fde331db9e2c
pascal.jura <boucnik.jiri@gmail.com>
2024-11-11 20:26:05 +0100

    detaily

--- test/index.php
+++ test/index.php
@@ -34,7 +34,7 @@ if (isset($_GET['id_zapis']) && filter_var($_GET['id_zapis'], FILTER_VALIDATE_IN
         $zapis = htmlspecialchars($radek['zapis']);
         $jmeno = htmlspecialchars($radek['username']);
 
-        // Nahrazení a formátování textu
+        // Formátování textu
         $zapis = str_replace("=", "\n", $zapis);
         $zapis = str_replace("\n--", "\n  ○", $zapis);
         $zapis = str_replace("\n-", "\n•", $zapis);
@@ -54,50 +54,50 @@ $phpWord = new PhpWord();
 $phpWord->setDefaultFontName('Calibri');
 
 // Definování sekce
-$sekce = $phpWord->addSection();
+$sekce = $phpWord->addSection([
+    'marginTop' => 1000,
+    'marginBottom' => 1000,
+    'marginLeft' => 1000,
+    'marginRight' => 1000
+]);
 
 // Přidání hlavičky
 $hlavicka = $sekce->addHeader();
-
-// Přidání obrázku do hlavičky, zarovnaného na střed
 $hlavicka->addImage('../assets/img/purkynka_logo.png', [
     'width' => 320,
     'height' => 103,
-    'alignment' => \PhpOffice\PhpWord\SimpleType\Jc::CENTER // Zarovnání na střed
+    'alignment' => \PhpOffice\PhpWord\SimpleType\Jc::CENTER
 ]);
 
-// Vytvoření tabulky v hlavičce s dvěma řádky a třemi sloupci
-$tableHeader = $hlavicka->addTable(['alignment' => \PhpOffice\PhpWord\SimpleType\Jc::CENTER, 'width' => 100 * 100]); // Tabulka na střed přes celou šířku stránky
+// Vytvoření tabulky v hlavičce
+$tableHeader = $hlavicka->addTable(['alignment' => \PhpOffice\PhpWord\SimpleType\Jc::CENTER, 'width' => 100 * 100]);
 $tableHeader->addRow();
-$tableHeader->addCell(4000, ['borderTopSize' => 6, 'borderTopColor' => '000000'])->addText("Číslo dokumentu: $cislo_dokumentu/$datum", ['size' => 9]);
+$tableHeader->addCell(4000, ['borderTopSize' => 6, 'borderTopColor' => '000000'])->addText(
+    "Číslo dokumentu: $cislo_dokumentu/$datum", ['size' => 9]
+);
+$tableHeader->addCell(2000, ['borderTopSize' => 6, 'borderTopColor' => '000000'])->addText(
+    "Počet stran: {PAGE} z {NUMPAGES}", ['size' => 9], ['alignment' => \PhpOffice\PhpWord\SimpleType\Jc::CENTER]
+);
+$tableHeader->addCell(2000, ['borderTopSize' => 6, 'borderTopColor' => '000000'])->addText(
+    "Počet příloh: 0", ['size' => 9], ['alignment' => \PhpOffice\PhpWord\SimpleType\Jc::RIGHT]
+);
 
-$tableHeader->addCell(2000, ['borderTopSize' => 6, 'borderTopColor' => '000000'])
-    ->addText("Počet stran: 1", ['size' => 9, 'alignment' => \PhpOffice\PhpWord\SimpleType\Jc::CENTER]);
-
-$tableHeader->addCell(2000, ['borderTopSize' => 6, 'borderTopColor' => '000000'])->addText("Počet příloh: 0", ['size' => 9, 'alignment' => \PhpOffice\PhpWord\SimpleType\Jc::RIGHT]);
-$tableHeader->addRow();
-$tableHeader->addCell(4000)->addText("Dokument", ['size' => 9]);
-$tableHeader->addCell(2000)->addText("", ['size' => 9, 'alignment' => 'center']);
-$tableHeader->addCell(2000)->addText("", ['size' => 9, 'alignment' => 'right']);
-
-// Přidání obsahu dokumentu
-$sekce->addText("Záznam z jednání dne " . date('d.m.Y', strtotime($radek['datum'])), ['size' => 22]);
+// Obsah dokumentu
+$sekce->addText("Záznam z jednání dne " . date('d.m.Y', strtotime($radek['datum'])), ['size' => 22, 'bold' => true]);
 $sekce->addText($zapis, ['size' => 11]);
 $sekce->addText("V Brně dne " . date('d.m.Y', strtotime($radek['datum'])), ['size' => 11]);
 $sekce->addText("Zástupci školního Parlamentu", ['size' => 11]);
 $sekce->addText("Zapsal: $jmeno", ['size' => 11]);
 $sekce->addText("Ověřila: Mgr. Denisa Gottwaldová", ['size' => 11]);
 
-// Vytvoření tabulky v zápatí, která bude také přes celou šířku stránky
+// Přidání zápatí
 $zapati = $sekce->addFooter();
-$tableFooter = $zapati->addTable(['alignment' => \PhpOffice\PhpWord\SimpleType\Jc::CENTER, 'width' => 100 * 100]); // Tabulka na střed přes celou šířku stránky
+$tableFooter = $zapati->addTable(['alignment' => \PhpOffice\PhpWord\SimpleType\Jc::CENTER, 'width' => 100 * 100]);
 $tableFooter->addRow();
-$tableFooter->addCell(4000, ['alignment' => 'left'])->addText("$cislo_dokumentu Záznam z jednání dne " . date('d.m.Y', strtotime($radek['datum'])), ['size' => 9]);
+$tableFooter->addCell(4000)->addText("$cislo_dokumentu Záznam z jednání dne " . date('d.m.Y', strtotime($radek['datum'])), ['size' => 9]);
+$tableFooter->addCell(2000)->addText("Stránka {PAGE} z {NUMPAGES}", ['size' => 9], ['alignment' => \PhpOffice\PhpWord\SimpleType\Jc::RIGHT]);
 
-// Vložení čísla stránky a celkového počtu stránek
-$tableFooter->addCell(2000, ['alignment' => 'right'])->addText("Stránka {PAGE} z {NUMPAGES}", ['size' => 9]);
-
-// Uložení souboru
+// Uložení souboru jako DOCX
 $nazevSouboru = 'zapis-ze-schuze-' . date('d-m-Y', strtotime($radek['datum'])) . '.docx';
 header("Content-Description: File Transfer");
 header('Content-Disposition: attachment; filename="' . $nazevSouboru . '"');
@@ -107,7 +107,9 @@ header('Expires: 0');
 header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
 header('Pragma: public');
 
+// Změna writeru na formát DOCX
 $writer = IOFactory::createWriter($phpWord, 'Word2007');
 $writer->save("php://output");
 exit();
+
 ?>
