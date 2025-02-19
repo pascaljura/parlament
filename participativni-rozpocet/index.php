663bdf8120a7990f728455110fd09adad334ce71
pascal.jura <boucnik.jiri@gmail.com>
2025-02-19 17:04:58 +0100

    test

---
+++ participativni-rozpocet/index.php
@@ -0,0 +1,34 @@
+<?php
+include '../assets/php/config.php';
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <link rel="shortcut icon" href="../favicon.ico" type="image/x-icon">
+    <title>Alba-rosa.cz | Parlament na Purkyňce</title>
+    <meta content="Alba-rosa.cz | Parlament na Purkyňce" property="og:title" />
+    <meta content="https://www.alba-rosa.cz/" property="og:url" />
+    <meta content="https://www.alba-rosa.cz/parlament/logo.png" property="og:image" />
+    <meta content="#0f1523" data-react-helmet="true" name="theme-color" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js"></script>
+</head>
+
+<body>
+    <div id="calendar">
+        <h2>Prezentace projektů na Participativní rozpočet</h2>
+        <iframe src="https://drive.google.com/file/d/168pTjyNDMarfrWX2TLUfAszc3oERGpcq/preview" 
+        width="100%" height="600px"></iframe>
+        <a href="https://drive.google.com/uc?export=download&id=168pTjyNDMarfrWX2TLUfAszc3oERGpcq" target="_blank">
+    Stáhnout PDF
+</a>
+
+    </div>
+</body>
+
+</html>
