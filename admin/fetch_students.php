172d59035abb10cacdd6dbfbe41de484f5a50327
pascal.jura <boucnik.jiri@gmail.com>
2025-03-02 21:48:15 +0100

    oprava

--- admin/fetch_students.php
+++ admin/fetch_students.php
@@ -1,15 +1,15 @@
 <?php
 require '../assets/php/config.php';
 
-$id = isset($_GET['id']) ? (int) $_GET['id'] : 0;
+$idattendances_list_parlament = isset($_GET['idattendances_list_parlament']) ? (int) $_GET['idattendances_list_parlament'] : 0;
 $students = [];
 
-if ($id > 0) {
+if ($idattendances_list_parlament > 0) {
     $sql = "
         SELECT u.username, DATE_FORMAT(a.time, '%d.%m.%Y %H:%i:%s') AS time
         FROM attendances_alba_rosa_parlament a
         JOIN users_alba_rosa_parlament u ON a.idusers = u.idusers
-        WHERE a.idmeetings_parlament = $id
+        WHERE a.idattendances_list_parlament = $idattendances_list_parlament
         ORDER BY a.time ASC
     ";
 
